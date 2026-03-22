257c793a8a12c499df0419fb70522547d22fec56
Antonis Geralis <b3lievem@yandex.com>
2026-03-22 19:13:46 +0200

    missing

---
+++ tests/tsharedargs.nim
@@ -0,0 +1,71 @@
+import std / assertions
+import ".." / "src" / [smartcli]
+
+var
+  nifcArgc {.importc: "cmdCount".}: int32
+  nifcArgv {.importc: "cmdLine".}: ptr UncheckedArray[cstring]
+
+block:
+  nifcArgc = 5
+  const cargv = [
+    cstring"greeter",
+    cstring"--output=out.txt",
+    cstring"-v",
+    cstring"greet",
+    cstring"input.txt"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Greeter v0.1
+This program greets.
+
+Usage: greeter [options] greet|version INPUT
+
+Commands:
+  greet    Greets NAME
+  version  Displays version and quits
+
+Arguments:
+  INPUT  Input file
+
+Options:
+  --mode=fast|slow  Output mode
+  --output=FILE     Output file
+  -v, --verbose     Enable verbose output
+  -h, --help        Show this help and exit"""
+
+  assert options.input == "input.txt"
+  assert options.output == "out.txt"
+  assert options.verbose
+  assert $options.command == "cmdGreet"
+
+block:
+  nifcArgc = 4
+  const cargv = [
+    cstring"greeter",
+    cstring"--mode=slow",
+    cstring"greet",
+    cstring"input.txt"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Greeter v0.1
+This program greets.
+
+Usage: greeter [options] greet|version INPUT
+
+Commands:
+  greet    Greets NAME
+  version  Displays version and quits
+
+Arguments:
+  INPUT  Input file
+
+Options:
+  --mode=fast|slow  Output mode
+  --output=FILE     Output file
+  -v, --verbose     Enable verbose output
+  -h, --help        Show this help and exit"""
+
+  assert options.input == "input.txt"
+  assert $options.mode == "cliModeSlow"
